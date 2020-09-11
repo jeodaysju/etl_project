1edd8c3f2d416620c2957b2ff244280d1c8a7696
jeodaysju <jeodaysju@yahoo.com>
2020-09-10 21:35:37 -0500

    added sql and updated minor changes in jupyter

---
+++ schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE box_office (
+rank INT PRIMARY KEY,
+released INT,
+movie TEXT,
+total_box_office TEXT
+);
+
+CREATE TABLE streaming (
+id INT PRIMARY KEY,
+title TEXT,
+released INT,
+suggested_viewing_age TEXT,
+imdb TEXT,
+rotten_tomatoes TEXT,
+netflix TEXT,
+hulu TEXT,
+prime_video TEXT,
+disney_plus TEXT,
+directors TEXT,
+genres TEXT,
+language TEXT,
+runtime INT
+);
